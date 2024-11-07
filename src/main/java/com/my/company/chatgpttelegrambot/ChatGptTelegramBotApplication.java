f960b64b185fb58350d5affd17413a7f86880f9c
Aleksandr Korshunovych <korsunovica@gmail.com>
2024-11-07 22:16:01 +0200

    init commit

---
+++ src/main/java/com/my/company/chatgpttelegrambot/ChatGptTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.my.company.chatgpttelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGptTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/company/chatgpttelegrambot/ChatGptTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.company.chatgpttelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

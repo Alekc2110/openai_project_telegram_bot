378499daecd1df3b74eb6f70a08b6dad5c06af50
Aleksandr Korshunovych <korsunovica@gmail.com>
2024-11-17 21:24:39 +0200

    ref. and optimized ChatGptHistoryCache

--- src/main/java/com/my/company/chatgpttelegrambot/domain/service/ChatGptHistoryCache.java
+++ src/main/java/com/my/company/chatgpttelegrambot/domain/service/ChatGptHistoryCache.java
@@ -3,32 +3,34 @@ package com.my.company.chatgpttelegrambot.domain.service;
 import com.my.company.chatgpttelegrambot.api.openai.model.ChatHistory;
 import com.my.company.chatgpttelegrambot.api.openai.model.Message;
 import lombok.RequiredArgsConstructor;
+import org.apache.el.util.ConcurrentCache;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
 public class ChatGptHistoryCache {
-    private final Map<Long, ChatHistory> chatHistoryMap = new ConcurrentHashMap<>();
+    private final ConcurrentCache<Long, ChatHistory> chatHistoryMap = new ConcurrentCache<>(100);
 
     public Optional<ChatHistory> getUserHistory(Long userId) {
         return Optional.ofNullable(chatHistoryMap.get(userId));
     }
 
     public void creatHistoryIfNotExist(Long userId) {
-        if (!chatHistoryMap.containsKey(userId)) {
-            chatHistoryMap.put(userId, new ChatHistory(new ArrayList<>()));
-        }
+        chatHistoryMap.put(userId, new ChatHistory(new ArrayList<>()));
     }
 
     public ChatHistory addMessageToHistory(Long userId, Message message) {
         ChatHistory chatHistory = getUserHistory(userId)
                 .orElseThrow(() -> new IllegalStateException("History does not exist for user =%s".formatted(userId)));
-        chatHistory.chatMessages().add(message);
+        List<Message> userList = chatHistory.chatMessages();
+        if (userList.size() > 30) {
+            userList.removeFirst();
+        }
+        userList.add(message);
         return chatHistory;
     }
 
